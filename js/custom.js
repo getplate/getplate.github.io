d88c973ec8965537e1491f6f139157b8b607a3b4
Jingun <its.jingun@gmail.com>
2015-01-20 18:06:38 +1300

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+var daysText = document.getElementById("days");
+var hoursText = document.getElementById("hours");
+var minutesText = document.getElementById("minutes");
+var secondsText = document.getElementById("seconds");
+
+var targetDate = new Date(2015, 01, 23);
+
+function updateText() {
+  var timespan = countdown(targetDate);
+  var months = timespan['months'];
+  var days = timespan['days'];
+  if (months == 1) {
+    days += 31; // Hardcoded 31 value for January
+  }
+  var hours = timespan['hours'];
+  var minutes = timespan['minutes'];
+  var seconds = timespan['seconds'];
+  daysText.innerHTML = days;
+  hoursText.innerHTML = hours;
+  minutesText.innerHTML = minutes;
+  secondsText.innerHTML = seconds;
+}
+
+updateText();
+
+setInterval(function(){
+  updateText()
+}, 1000);
